46f384ba453bfb19346e08a28acb80709997eea2
krim <pasha.rod@mail.ru>
2018-06-26 22:42:18 +0300

    private methods

--- js/views/modals/error.js
+++ js/views/modals/error.js
@@ -3,16 +3,16 @@ import AbstractView from '../abstract-view';
 export default class ErrorModalView extends AbstractView {
   constructor(data, errorDescription) {
     super();
-    this.data = data;
-    this.errorDescription = errorDescription;
+    this._data = data;
+    this._errorDescription = errorDescription;
   }
 
   get template() {
     return `
       <section class="modal-error modal-error__wrap">
         <div class="modal-error__inner">
-          <h2 class="modal-error__title">${this.data.title}</h2>
-          <p class="modal-error__text">${this.errorDescription}. ${this.data.description}</p>
+          <h2 class="modal-error__title">${this._data.title}</h2>
+          <p class="modal-error__text">${this._errorDescription}. ${this._data.description}</p>
         </div>
       </section>
     `;
